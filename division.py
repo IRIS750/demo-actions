89ad9907b2362eceafba1b33f30e1593ecd8753a
IRIS750 <vivaldez001form@gmail.com>
2024-01-22 23:26:43 +0100

    correcciones

--- division.py
+++ division.py
@@ -2,7 +2,4 @@ def dividir(a, b):
     if b != 0:
         return a / b
     else:
-        print("Error: No se puede dividir por cero.")
-        
-if __name__ == "__main__":
-    print(dividir(6, 2))
+        raise ZeroDivisionError("No se puede dividir por cero.")
